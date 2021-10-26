8eb37f33517afc50c4b7d4857ec50ac1d0b0bfe7
ilya <ZiK201091@ya.ru>
2021-10-26 13:31:12 +0700

    Bot chat meat day

---
+++ src/main/java/ru/ilya/botmeatday/telegrambotmeatday/TelegramBotMeatDayApplication.java
@@ -0,0 +1,13 @@
+package ru.ilya.botmeatday.telegrambotmeatday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotMeatDayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotMeatDayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ilya/botmeatday/telegrambotmeatday/TelegramBotMeatDayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ilya.botmeatday.telegrambotmeatday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotMeatDayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
